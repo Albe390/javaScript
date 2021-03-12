0366199177b68e131e6d271cab88dc0a3e83a31d
Albert <example@gmail.com>
2021-03-13 00:51:49 +0300

    add in js

--- js/jscript.js
+++ js/jscript.js
@@ -1,3 +1,7 @@
 'usestrict';
 let person = { name: 'pedro', apellido: 'saudedo', age: 34 };
 console.log(person);
+if (name != 'pedro') {
+    console.log("nado pisat imia");
+    console.log('ASV');
+}
